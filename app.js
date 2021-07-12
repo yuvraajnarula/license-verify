63909a74daa1cfee0667a1a30187bf3b6c8e205a
unknown <ynarula678@gmail.com>
2021-07-12 22:37:15 +0530

    'js'

---
+++ app.js
@@ -0,0 +1,20 @@
+const [img_1, img_2, img_3] = [document.querySelector('#img-1'), document.querySelector('#img-2'), document.querySelector('#img-3')];
+const [btn_1, btn_2, btn_3] = [document.querySelector('#btn-1'), document.querySelector('#btn-2'), document.querySelector('#btn-3')];
+
+btn_1.addEventListener('click',()=>{
+    img_1.style.display = 'block';
+    img_2.style.display = 'none';
+    img_3.style.display = 'none';
+
+})
+btn_2.addEventListener('click',()=>{
+    img_1.style.display = 'none';
+    img_2.style.display = 'block';
+    img_3.style.display = 'none';
+})
+btn_3.addEventListener('click',()=>{
+    img_1.style.display = 'none';
+    img_2.style.display = 'none';
+    img_3.style.display = 'block';
+    
+})
