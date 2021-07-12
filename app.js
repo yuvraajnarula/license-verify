3b157d6d8cafc21ccd89efc3c22b5569a31e77ff
unknown <ynarula678@gmail.com>
2021-07-13 00:14:16 +0530

    'btn'

--- app.js
+++ app.js
@@ -64,14 +64,23 @@ sol1.addEventListener('click', ()=>{
     para1.classList.add('active-para');
     para2.classList.remove('active-para');
     para3.classList.remove('active-para');
+    sol1.style.color = '#6097E2';
+    sol2.style.color = '#ECF2FA';
+    sol3.style.color = '#ECF2FA';
 })
 sol2.addEventListener('click',()=>{
     para1.classList.remove('active-para');
     para2.classList.add('active-para');
     para3.classList.remove('active-para');
+    sol2.style.color = '#6097E2';
+    sol1.style.color = '#ECF2FA';
+    sol3.style.color = '#ECF2FA';
 })
 sol3.addEventListener('click', ()=>{
     para1.classList.remove('active-para');
     para2.classList.remove('active-para');
     para3.classList.add('active-para');
+    sol3.style.color = '#6097E2';
+    sol1.style.color = '#ECF2FA';
+    sol2.style.color = '#ECF2FA';
 })
