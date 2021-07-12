42887374c3b327f5bddcb83823050bb5f928057f
unknown <ynarula678@gmail.com>
2021-07-12 23:57:33 +0530

    'para'

--- app.js
+++ app.js
@@ -32,4 +32,22 @@ modal_btn.addEventListener('click', ()=>{
 close.addEventListener('click',()=>{
     modal.style.display='none';
 })
-//boxes swiper
+//solution tabs
+const [para1, para2, para3] =[ document.querySelector('#para1'), document.querySelector('#para2'), document.querySelector('#para3')];
+const [sol1 , sol2 , sol3 ] = [ document.querySelector('#sol1'), document.querySelector('#sol2'), document.querySelector('#sol3')];
+
+sol1.addEventListener('click', ()=>{
+    para1.classList.add('active-para');
+    para2.classList.remove('active-para');
+    para3.classList.remove('active-para');
+})
+sol2.addEventListener('click',()=>{
+    para1.classList.remove('active-para');
+    para2.classList.add('active-para');
+    para3.classList.remove('active-para');
+})
+sol3.addEventListener('click', ()=>{
+    para1.classList.remove('active-para');
+    para2.classList.remove('active-para');
+    para3.classList.add('active-para');
+})
